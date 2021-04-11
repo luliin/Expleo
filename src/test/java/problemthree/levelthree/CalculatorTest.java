ecd22cc338873c3d3757c6a3069eb02cc356edb4
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2021-04-11 16:01:23 +0200

    Add test folder and test Class for Level 3

---
+++ src/test/java/problemthree/levelthree/CalculatorTest.java
@@ -0,0 +1,11 @@
+package problemthree.levelthree;
+
+/**
+ * Created by Julia Wigenstedt
+ * Date: 2021-04-11
+ * Time: 16:00
+ * Project: Expleo
+ * Copyright: MIT
+ */
+public class CalculatorTest {
+}
